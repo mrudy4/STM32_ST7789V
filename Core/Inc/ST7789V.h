9deb94a329a26a6d61751a300e57e417d23ae332
mrudy4 <259581@student.pwr.edu.pl>
2024-06-18 22:29:23 +0200

    init

---
+++ Core/Inc/ST7789V.h
@@ -0,0 +1,13 @@
+/*
+ * ST7789V.h
+ *
+ *  Created on: Jun 13, 2024
+ *      Author: maciejrudy
+ */
+
+#ifndef INC_ST7789V_H_
+#define INC_ST7789V_H_
+
+void ST7789V_init();
+
+#endif /* INC_ST7789V_H_ */
---
+++ Core/Src/ST7789V.c
@@ -0,0 +1,33 @@
+/*
+ * ST7789V.c
+ *
+ *  Created on: Jun 13, 2024
+ *      Author: maciejrudy
+ */
+
+#include "stm32f1xx_hal.h"
+#include "gpio.h"
+#include "ST7789V.h"
+
+extern SPI_HandleTypeDef hspi2;
+#define ST7789V_SPI &hspi2
+#define ST7789V_CS_PORT GPIOB
+#define ST7789V_CS_PIN CS_Pin
+
+void ST7789V_init(){
+
+	HAL_GPIO_WritePin(ST7789V_CS_PORT, ST7789V_CS_PIN, 0);
+
+}
+
+void CS_Select (void)
+{
+	HAL_GPIO_WritePin(ST7789V_CS_PORT, ST7789V_CS_PIN, GPIO_PIN_RESET);
+}
+
+void CS_UnSelect (void)
+{
+	HAL_GPIO_WritePin(ST7789V_CS_PORT, ST7789V_CS_PIN, GPIO_PIN_SET);
+}
+
+
